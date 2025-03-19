162a429942ae5e96230e1a8dc73a3c4f3d60f0d4
MatteoNeri99 <matteolavoro99@gmail.com>
2025-03-19 09:42:36 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script>
+import MainApp from './components/MainApp.vue'
+import FooterApp from './components/FooterApp.vue'
+import HeaderApp from './components/HeaderApp.vue'
+export default{
+  components:{
+    HeaderApp,
+    MainApp,
+    FooterApp
+  },
+
+  data(){
+    return{
+
+    }
+  }
+}
+
+</script>
+
+<template>
+  <HeaderApp/>
+  <MainApp/>
+  <FooterApp/>
+</template>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+</style>
